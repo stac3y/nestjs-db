37f14a0e03c995e61ef1b06bec7bd62de78bbdfe
stac3y <stacy.starina@gmail.com>
2021-11-07 23:44:26 +0300

    feat: add tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,20 +1,18 @@
 import { performance } from 'perf_hooks'
 
-
 import { INestApplication, Logger } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
+import { MongooseModule } from '@nestjs/mongoose'
 import { Test, TestingModule } from '@nestjs/testing'
-import { TypeOrmModule } from '@nestjs/typeorm'
 import * as request from 'supertest'
 
-import { typeOrmConfig } from 'src/config/typeorm.config'
+import { mongooseConfig } from 'src/config/mongoose.config'
 import { UsersModule } from 'src/users/users.module'
 
-import { AppModule } from '../src/app.module'
 import { config } from '../src/config/config'
 
-const TIMEOUT = 3000
+const TIMEOUT = 10000
 
 describe('AppController (e2e)', () => {
     let app: INestApplication
@@ -23,54 +21,86 @@ describe('AppController (e2e)', () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [
                 ConfigModule.forRoot(config),
-                TypeOrmModule.forRootAsync(typeOrmConfig),
+                MongooseModule.forRootAsync(mongooseConfig),
                 GraphQLModule.forRoot({
                     installSubscriptionHandlers: true,
                     autoSchemaFile: 'schema.gql',
-                    playground: true,
                     introspection: true,
                 }),
                 UsersModule,
-                AppModule,
             ],
         }).compile()
 
         app = moduleFixture.createNestApplication()
         await app.init()
     })
+    describe('MongoDB Test (e2e)', () => {
+        it('writing', async () => {
+            const configService = app.get(ConfigService)
+            const AMOUNT = configService.get('AMOUNT')
+            const begin = performance.now()
+            const response = await request(app.getHttpServer())
+                .post('/graphql')
+                .send({
+                    query: `mutation createEntities {
+                        createEntities(input: { amount: ${AMOUNT} }) {
+                          message
+                        }
+                      }`,
+                })
+            const end = performance.now()
+            const time = end - begin
+            Logger.log(response.body)
+            Logger.log(`Time for writing: ${time} ms`)
+            return response
+        })
 
-    it('writing', async () => {
-        const configService = app.get(ConfigService)
-        const AMOUNT = configService.get('AMOUNT')
-        const begin = performance.now()
-        const response = await request(app.getHttpServer())
-            .post('/graphql')
-            .send({
-                mutation: `{
-                createEntities(input: { amount: ${AMOUNT} })
-              }`,
-            })
-        const end = performance.now()
-        const time = end - begin
-        Logger.log(response.body.data)
-        Logger.log(`Time for writing: ${time} ms`)
-        return expect(response).not.toBeUndefined()
+        it('reading', async () => {
+            const configService = app.get(ConfigService)
+            const AMOUNT = configService.get('AMOUNT')
+            const begin = performance.now()
+            const response = await request(app.getHttpServer())
+                .post('/graphql')
+                .send({
+                    query: `query getEntities {
+                        getEntities(input: { amount: ${AMOUNT} }) {
+                          result {
+                            user {
+                              name
+                              surname
+                              login
+                              password
+                            }
+                            status {
+                              name
+                            }
+                            role {
+                              name
+                            }
+                            group {
+                              name
+                              shortname
+                            }
+                            joke {
+                              name
+                              text
+                              rate
+                              view
+                              like
+                            }
+                            level {
+                              name
+                            }
+                          }
+                        }
+                      }`,
+                })
+                .set('Accept', 'application/json')
+            const end = performance.now()
+            const time = end - begin
+            Logger.log(response.body)
+            Logger.log(`Time for reading: ${time} ms`)
+            return expect(response).not.toBeUndefined()
+        })
     })
-
-     it('reading', async () => {
-        const configService = app.get(ConfigService)
-        const AMOUNT = configService.get('AMOUNT')
-        const begin = performance.now()
-        const response = await request(app.getHttpServer())
-            .post('/graphql')
-            .send({
-                query: `{
-                getEntities(input: { amount: ${AMOUNT} }{result})
-              }`,
-            })
-        const end = performance.now()
-        const time = end - begin
-        Logger.log(`Time for reading: ${time} ms`)
-        return expect(response).not.toBeUndefined()
-    }) 
 })
